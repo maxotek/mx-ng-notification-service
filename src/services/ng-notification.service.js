3c122fb18cace690837e1ee956fd4f70db482eff
partho <partho@maxotek.com>
2018-02-24 15:28:41 +0530

    Added angular module

---
+++ src/services/ng-notification.service.js
@@ -0,0 +1,53 @@
+(function(app) {
+    "use strict";
+
+    app.factory("mxngNotificationService", mxngNotificationService);
+
+    mxngNotificationService.$inject = ["toastr"];
+
+    function notificationService(toastr) {
+        function displaySuccess(message) {
+            toastr.success(message);
+        }
+
+        function displayError(error) {
+            if (Array.isArray(error)) {
+                error.forEach(function(err) {
+                    toastr.error(err);
+                });
+            } else {
+                toastr.error(error, "Error");
+            }
+        }
+
+        function displayWarning(message) {
+            toastr.warning(message);
+        }
+
+        function displayInfo(message) {
+            toastr.info(message);
+        }
+
+        function handleDataError(response) {
+            console.log(response);
+
+            if (!response.data)
+                return displayError("Unable to connect to API server");
+
+            var error = response.data.message || response.data.error_description;
+            if (error)
+                displayError(error);
+            else
+                displayError(response.statusText);
+        }
+
+        return {
+            displaySuccess: displaySuccess,
+            displayError: displayError,
+            displayWarning: displayWarning,
+            displayInfo: displayInfo,
+            handleDataError: handleDataError
+        };
+    }
+
+})(angular.module("mxngNotification"));
